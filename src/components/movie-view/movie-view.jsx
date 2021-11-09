fc70f042d6988bcb2db939364d8c88d17c25290f
ramirune <timpiga3@hotmail.com>
2021-11-09 15:17:44 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,17 +2,33 @@ import React from "react";
 
 export class MovieView extends React.Component {
 
+  keypressCallback(event) {
+    console.log(event.key);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keypress', this.keypressCallback);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keypress', this.keypressCallback);
+  }
+
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src= "{movie.ImagePath}" />
+          <img src= {movie.ImagePath} />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
           <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
         </div>
         <div className="movie-description">
           <span className="label">Description: </span>
