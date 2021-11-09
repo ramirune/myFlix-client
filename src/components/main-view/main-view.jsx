19a4af2c8fe5b7dbd123f242813fa39dd8f090e4
ramirune <timpiga3@hotmail.com>
2021-11-09 15:17:34 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import axios from 'axios';
+
+import { LoginView } from "../login-view/login-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -7,41 +10,49 @@ export class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
-      movies: [
-        { _id: 1,
-          Title: 'Howl\'s Moving Castle', 
-          Description: 'When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle.',
-          ImagePath: './img/howlMovingCastle.jpg'},
-        { _id: 2,
-          Title: 'Frozen',
-          Description: 'When the newly crowned Queen Elsa accidentally uses her power to turn things into ice to curse her home in infinite winter, her sister Anna teams up with a mountain man, his playful reindeer, and a snowman to change the weather condition.',
-          ImagePath: '...'},
-        { _id: 3,
-          Title: 'Spirited Away',
-          Description: 'During her family\'s move to the suburbs, a sullen 10-year-old girl wanders into a world ruled by gods, witches, and spirits, and where humans are changed into beasts.',
-          ImagePath: '...'}
-      ],
-      selectedMovie: null
+      movies: [],
+      selectedMovie: null,
+      user: null
     };
+  }
+
+  componentDidMount(){
+    axios.get('https://movie-api-by-tammy.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
     this.setState({
-      selectedMovie: newSelectedMovie
+      selectedMovie: Movie
+    });
+  }
+
+  onLoggedIn(user) {
+    this.setState({
+      user
     });
   }
 
   render(){
     const { movies, selectedMovie } = this.state;
 
-    if(movies.length === 0) return <div className = "main-view">The list is empty!</div>;
+    if(!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    if(movies.length === 0) return <div className = "main-view" />;
    
     return (
         <div className = "main-view">
           {selectedMovie
         ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
         : movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+          <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
         ))
       }
         </div>
