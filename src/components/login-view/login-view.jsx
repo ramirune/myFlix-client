0767e29d85374f711345c0f6bdaf168712e8b00f
ramirune <timpiga3@hotmail.com>
2021-11-11 23:58:19 +0100

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,33 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Card, Button, Form, Row, Col, Container } from "react-bootstrap";
+import "./login-view.scss";
 
 export function LoginView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
+    console.log(Username, Password);
     /* Send a request to the server for authentication */
     /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    props.onLoggedIn(Username);
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} required/>
-      </label>
-      <br />
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} required/>
-      </label>
-      <br />
-      <br />
-      <button type="submit" onClick={handleSubmit}>Log in</button>
+    <Container>
+      <Row>
+        <Col></Col>
+        <Col>
+          <Card className="loginCard" bg="dark">
+            <Form>
+              <Form.Group controlId="formUsername">
+                <Form.Label style={{ color: "white" }}>Username:</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Form.Group>
 
-    </form>
+              <Form.Group controlId="formPassword">
+                <Form.Label style={{ color: "white" }}>Password:</Form.Label>
+                <Form.Control
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </Form.Group>
+              <br />
+              <Button variant="warning" type="submit" onClick={handleSubmit}>
+                Log In
+              </Button>
+            </Form>
+          </Card>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
   );
 }
+
+LoginView.propTypes = {
+  user: PropTypes.shape({
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+  }),
+  onLoggedIn: PropTypes.func.isRequired,
+};
