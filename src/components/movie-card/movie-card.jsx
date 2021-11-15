38aaf2a9e0b3f0b876654db14ec8a04b841fb8eb
ramirune <timpiga3@hotmail.com>
2021-11-15 19:38:02 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { CardGroup, Container, Row, Button, Card, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
 export class MovieCard extends React.Component {
@@ -8,30 +9,36 @@ export class MovieCard extends React.Component {
     const { movie, onMovieClick } = this.props;
 
     return (
-      <Container style={{ display: "inline" }}>
-        <Row>
-          <Col>
-            <CardGroup>
-              <Card className="poster" bg="dark">
-                <Card.Img variant="top" src={movie.ImagePath} height="350" />
-                <Card.Body>
-                  <Card.Title className="movie_title">{movie.Title}</Card.Title>
-                  <div className="movie_genre">{movie.Genre.Name}</div>
-                  {/* <Card.Text>{movie.Description}</Card.Text> */}
-                  <Button
-                    className="button"
-                    variant="warning"
-                    size="md"
-                    onClick={() => onMovieClick(movie)}
-                  >
-                    Open
-                  </Button>
-                </Card.Body>
-              </Card>
-            </CardGroup>
-          </Col>
-        </Row>
-      </Container>
+      <div className="mvcard">
+        <Container className="moviecardContainer" style={{ display: "inline" }}>
+          <Row>
+            <Col>
+              <CardGroup>
+                <Card className="poster">
+                  <Card.Img
+                    className="movie-img"
+                    variant="top"
+                    src={movie.ImagePath}
+                    height="350"
+                  />
+                  <Card.Body>
+                    <Card.Title className="movie_title">
+                      {movie.Title}
+                    </Card.Title>
+                    <div className="movie_genre">{movie.Genre.Name}</div>
+                    {/* <Card.Text>{movie.Description}</Card.Text> */}
+                    <Link to={`/movies/${movie.Title}`}>
+                      <Button className="button" variant="danger" size="md">
+                        Open
+                      </Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </CardGroup>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
@@ -44,5 +51,4 @@ MovieCard.propTypes = {
     ImagePath: PropTypes.string.isRequired,
     //Genre: PropTypes.string,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
 };
