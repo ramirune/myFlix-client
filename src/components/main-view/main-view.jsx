8320237fdf1f80a5df640c53a284c3090572a1ba
ramirune <timpiga3@hotmail.com>
2021-11-15 19:37:58 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,8 +5,18 @@ import { LoginView } from "../login-view/login-view";
 import { RegistrationView } from "../registration-view/registration-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { NavBarView } from "../navbar-view/navbar-view";
+//import { GenreView } from "../genre-view/genre-view";
+//import { DirectorView } from "../director-view/director-view";
+import { ProfileView } from "../profile-view/profile-view";
 
 import { Container, Col, Row } from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import "./main-view.scss";
 
 export class MainView extends React.Component {
@@ -19,17 +29,30 @@ export class MainView extends React.Component {
     };
   }
 
-  componentDidMount() {
+  getMovies(token) {
     axios
-      .get("https://movie-api-by-tammy.herokuapp.com/movies")
+      .get(`https://movie-api-by-tammy.herokuapp.com/movies`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
       .then((response) => {
+        // Assign the result to the state
         this.setState({
           movies: response.data,
         });
       })
-      .catch((error) => {
+      .catch(function (error) {
         console.log(error);
       });
+  }
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem("token");
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem("user"),
+      });
+      this.getMovies(accessToken);
+    }
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -44,54 +67,102 @@ export class MainView extends React.Component {
     });
   }
 
-  onLoggedIn(user) {
+  onLoggedIn(authData) {
+    console.log(authData);
     this.setState({
-      user,
+      user: authData.user.Username,
+    });
+
+    localStorage.setItem("token", authData.token);
+    localStorage.setItem("user", authData.user.Username);
+    this.getMovies(authData.token);
+  }
+
+  onLoggedOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    this.setState({
+      user: null,
     });
   }
 
   render() {
-    const { movies, selectedMovie, user } = this.state;
-
-    if (!user)
-      return (
-        <div>
-          <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
-          <RegistrationView />
-        </div>
-      );
-
-    if (movies.length === 0) return <div className="main-view" />;
+    const { movies, user } = this.state;
 
     return (
-      <div className="main-view">
-        <Container fluid>
-          <Row className="justify-content-md-center">
-            {selectedMovie ? (
-              <Col md={6}>
-                <MovieView
-                  movie={selectedMovie}
-                  onBackClick={(newSelectedMovie) => {
-                    this.setSelectedMovie(newSelectedMovie);
-                  }}
-                />
-              </Col>
-            ) : (
-              movies.map((movie) => (
-                <Col md={3}>
-                  <MovieCard
-                    key={movie._id}
-                    movie={movie}
-                    onMovieClick={(newSelectedMovie) => {
-                      this.setSelectedMovie(newSelectedMovie);
-                    }}
-                  />
-                </Col>
-              ))
-            )}
-          </Row>
-        </Container>
-      </div>
+      <Router>
+        <NavBarView />
+        <>
+          <Container className="body">
+            <Row className="justify-content-md-center">
+              <Route
+                exact
+                path="/"
+                render={() => {
+                  if (!user)
+                    return (
+                      <Col>
+                        <LoginView
+                          onLoggedIn={(user) => this.onLoggedIn(user)}
+                        />
+                        <RegistrationView />
+                      </Col>
+                    );
+
+                  if (movies.length === 0) return <div className="main-view" />;
+                  return movies.map((m) => (
+                    <Col md={3} key={m._id}>
+                      <MovieCard movie={m} />
+                    </Col>
+                  ));
+                }}
+              />
+              <Route
+                path="/movies/:Title"
+                render={({ match, history }) => {
+                  if (!user)
+                    return (
+                      <Col>
+                        <LoginView
+                          onLoggedIn={(user) => this.onLoggedIn(user)}
+                        />
+                      </Col>
+                    );
+                  if (movies.length === 0) return <div className="main-view" />;
+                  return (
+                    <Col md={8}>
+                      <MovieView
+                        movie={movies.find(
+                          (m) => m.Title === match.params.Title
+                        )}
+                        onBackClick={() => history.goBack()}
+                      />
+                    </Col>
+                  );
+                }}
+              />
+              <Route
+                exact
+                path="/user/:Username"
+                render={({ history }) => {
+                  if (!user)
+                    return (
+                      <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                    );
+                  if (movies.length === 0) return <div className="main-view" />;
+                  return (
+                    <ProfileView
+                      movies={movies}
+                      user={user}
+                      onBackClick={() => history.goBack()}
+                    />
+                  );
+                }}
+              />
+            </Row>
+          </Container>
+        </>
+      </Router>
     );
   }
 }
