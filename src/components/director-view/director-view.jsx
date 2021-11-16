86457c89cf517d8491740f7c6ab427979273bc11
ramirune <timpiga3@hotmail.com>
2021-11-16 17:44:48 +0100

    Update director-view.jsx

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,34 +1,60 @@
 import React from "react";
-import axios from "axios";
 import PropTypes from "prop-types";
-import { Container, Row, Col, Card } from "react-bootstrap";
+
+import { Container, Card, Button } from "react-bootstrap";
 
 import "./director-view.scss";
 
 export class DirectorView extends React.Component {
-  constructor() {
-    super();
-  }
-
   render() {
-    const { director, onBackClick } = this.props;
+    const { Director, onBackClick } = this.props;
 
     return (
-      <Container className="director-view">
-        <Row>
-          <Col>
-            <Card>
-              <Card.Body>
-                <Card.Title>{director.Name}</Card.Title>
-                <Card.Text>
-                  <span className="label">Bio:</span>
-                  <span className="value">{director.Bio}</span>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+      <Container className="director-container">
+        <Card>
+          <Card.Header>
+            <h4>Director</h4>
+          </Card.Header>
+          <Card.Body>
+            <div>
+              <span className="label">Name: </span>
+              <span className="value">{Director.Name}</span>
+            </div>
+            <div>
+              <span className="label">Bio: </span>
+              <span className="value">{Director.Bio}</span>
+            </div>
+            <div>
+              <span className="label">Born: </span>
+              <span className="value">{Director.Birth}</span>
+            </div>
+            <div>
+              <span className="label">Death: </span>
+              <span className="value">{Director.Death}</span>
+            </div>
+            <div className="backButton">
+              <Button
+                size="md"
+                variant="warning"
+                onClick={() => {
+                  onBackClick(null);
+                }}
+              >
+                Back
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
       </Container>
     );
   }
 }
+
+DirectorView.proptypes = {
+  Director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string,
+    Birth: PropTypes.number,
+    Death: PropTypes.number,
+  }).isRequired,
+};
